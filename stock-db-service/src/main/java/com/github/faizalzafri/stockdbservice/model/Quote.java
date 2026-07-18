560d811f3de29efa60ca98a605e106a790b017bc
Faizal <34762675+faizalzafri@users.noreply.github.com>
2026-07-18 19:17:10 +0530

    Implement database evolution. CLoses #21

--- stock-db-service/src/main/java/com/github/faizalzafri/stockdbservice/model/Quote.java
+++ stock-db-service/src/main/java/com/github/faizalzafri/stockdbservice/model/Quote.java
@@ -12,7 +12,7 @@ import jakarta.persistence.Table;
 public class Quote {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
 
